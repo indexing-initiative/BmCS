eb034921cd96b4d96bd41f42090729f10adc40f2
Max Savery <saverymax@gmail.com>
2019-06-27 15:28:06 -0400

    instructions for making package

--- make_package.sh
+++ make_package.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# For Max's use only
+# For Local use only, to make package. Instructions not included in README
+# Must have BmCS_installation_env directory
 rm -r build
 rm -r dist
 python setup.py bdist_wheel
